87e805a3c1350b1a8570ff061f2b382d7b2bda52
jdstigma <98896124+jdstigma@users.noreply.github.com>
2026-05-12 23:28:04 +0000

    updating comments

--- PostgreSQL Database.session.sql
+++ PostgreSQL Database.session.sql
@@ -1,2 +1,5 @@
 /* If this file is empty, it means the postgres script is not currently in use.
-File usage is for interacting using postgres shell, and is not used by the application itself. */ 
+File usage is for interacting using postgres shell, and is not used by the application itself. 
+Current usage is for testing and debugging purposes, and may be used in the future for database management.
+Specific usage would be using the explain and analyze commands
+*/ 
