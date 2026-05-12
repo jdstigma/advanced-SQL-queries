23de7ff2e8dd2eda7062fce156effd87d4b60467
jdstigma <98896124+jdstigma@users.noreply.github.com>
2026-05-12 23:31:13 +0000

    more comments

--- PostgreSQL Database.session.sql
+++ PostgreSQL Database.session.sql
@@ -2,4 +2,5 @@
 File usage is for interacting using postgres shell, and is not used by the application itself. 
 Current usage is for testing and debugging purposes, and may be used in the future for database management.
 Specific usage would be using the explain and analyze commands
+This would be where you would write any postgres commands you want to run, and then you can run this file in the postgres shell to execute those commands.
 */ 
