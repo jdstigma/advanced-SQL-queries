c68d4d07c56a8178f2885d3d608b0d6b6d99d4b4
jdstigma <98896124+jdstigma@users.noreply.github.com>
2026-05-12 02:44:25 +0000

    trying different network setup

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "127.0.0.11 $(hostname)" | sudo tee -a /etc/hosts > /dev/null
+echo "127.0.0.1 $(localhost)" | sudo tee -a /etc/hosts > /dev/null
 
 echo "Waiting for PostgreSQL..."
-for i in $(seq 1 30); do
+for i in $(seq 1 60); do
   psql -h db -U postgres postgres -c '\q' 2>/dev/null && break
-  echo "Attempt $i/30 - waiting..."
-  sleep 2
+  echo "Attempt $i/60 - waiting..."
+  sleep 10
 done
 
 if [ -f .devcontainer/setup-postgresql.sql ]; then
