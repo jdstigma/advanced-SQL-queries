80263fcc31777cd5bbe1e603373b04fa62bbfec7
jdstigma <98896124+jdstigma@users.noreply.github.com>
2026-05-14 23:07:24 +0000

    materialized view

--- PostgreSQL Database.session.sql
+++ PostgreSQL Database.session.sql
@@ -1,6 +1,1 @@
-/* If this file is empty, it means the postgres script is not currently in use.
-File usage is for interacting using postgres shell, and is not used by the application itself. 
-Current usage is for testing and debugging purposes, and may be used in the future for database management.
-Specific usage would be using the explain and analyze commands
-This would be where you would write any postgres commands you want to run, and then you can run this file in the postgres shell to execute those commands.
-*/ 
+refresh materialized view my_staff;
